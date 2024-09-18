1b8b6c6032546ab5c69a58ff40c36dce41f46b8f
DSM4 <fatec-dsm4@academico.intra>
2024-09-17 22:18:11 -0300

    service: updating login

--- app/service/UserService.py
+++ app/service/UserService.py
@@ -1,6 +1,8 @@
 from models.User import User
 from repository.UserRepository import UserRepository
 from sqlalchemy.exc import IntegrityError
+from utils.hash.password import verify_password
+from flask import session
 class UserService():
     def register(user: User):
         try:
@@ -11,5 +13,20 @@ class UserService():
         
         except Exception as e:
             return {"error": f"{e}"}
+    
+    def login(user: User):
+        try:
+            userRegistered = UserRepository.findByEmail(user.email)
+            if userRegistered and verify_password(userRegistered.password, user.password):
+                session['user_id'] = user.id
+                session['email'] = user.username
+                return {"message": "Usuário logado com sucesso"}
+            else:
+                return {"error": "Usuário email e/ou senhas incorretas ou usuário inexistente"}
+            
+            
+                
+                
+                     
         
             
