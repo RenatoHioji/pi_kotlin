dc5983536e1ae7b3d94c2a4037380275776dc83c
DSM4 <fatec-dsm4@academico.intra>
2024-10-31 21:40:49 -0300

    game: starting controller

--- app/models/Quiz.py
+++ app/models/Quiz.py
@@ -6,7 +6,6 @@ class Quiz(db.Model):
     __tablename__ = "quiz"
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     nivel = db.Column(db.Integer, unique=False, nullable=False)
-    
     quiz_game_list = db.relationship("Game", backref="game_owner", lazy=True)
 
     
